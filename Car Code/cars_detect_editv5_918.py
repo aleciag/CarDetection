16e48b1c6714a7bde18ae00a9b9eb77e003a08f2
Zachary Safran <zsafran@bu.edu>
2018-09-20 13:07:22 -0400

    Edited Comments

--- Car Code/cars_detect_editv5_918.py
+++ Car Code/cars_detect_editv5_918.py
@@ -1,4 +1,8 @@
-
+#
+# Final version of our live car counting script.
+#
+# Created by Zachary Safran and Alecia Griffin.
+#
 import cv2
 import math
 import numpy as np
@@ -6,19 +10,28 @@ import os
 import time
 import h5py
 
+# time.time() from the time library returns the current Epoch time
+# This was used inconjunction with the os library to create unique files
+# and directories each time the sript is run.
 start_time = str(math.floor(time.time()))
 start_path = '/home/pi/.virtualenvs/cv_wh/ProjectDocs/code/miniprojectData/' + start_time + '/'
 
+# Makes a new directory when the script starts at boot up
 if not os.path.exists(start_path):
     os.makedirs(start_path)
 
+# Changes paths to the newly created directory
 os.chdir( start_path )
 
+# Next the program runs in a continues loop capturing and processing ~5 mins of video
+# At the end of each loop a .avi video file and a .hdf5 data file are output.
+# This loop will run until the Pi is powered off or the program is killed by the user.
 while True:
     record_time = str(math.floor(time.time()))
 
     path = start_path + record_time + '/'
 
+    # A new folder is created to contain the files output by a given "loop cycle"
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -26,46 +39,60 @@ while True:
 
     filename = str(math.floor(time.time())) + '.avi'
 
+    # Video starts being captured and output .avi file is set up.
     cap = cv2.VideoCapture(0)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(path + filename,fourcc,fps=1,frameSize=(640,480))
 
+    # CascadeClassifier is set up to detect cars with the cars.xml classifier
     car_cascade = cv2.CascadeClassifier('/home/pi/.virtualenvs/cv_wh/ProjectDocs/code/Car Detection/cars.xml')
 
     count=0
     i = 0
+
+    # Arrays for data are preallocated.
     carcount_arr = np.zeros(240)
     time_arr = np.zeros(240)
 
     tstop = time.time() + 310
+    # Video is set to record for ~5mins.
     while time.time() < tstop:
 
+        # Each frame is captured individually.
         ret,frame=cap.read()
+        # Convert video into gray scale of each frames.
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        # Each frame is analyzed for cars.
         cars = car_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in cars:
+            # A BLUE rectangle is placed around the detected cars and the variable count is incremented.
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
             roi_color = frame[y:y+h, x:x+w]
             count+=1
+
+        # Text of the cummulative car count is added to each frame.
         cv2.putText(frame, str(count),(10,400), cv2.FONT_ITALIC, 2,(255,255,255),2,cv2.LINE_AA)
+        # The frame is written to the .avi output file.
         out.write(frame)
         # The following line is commented out to allow this sript to run without a display
         # cv2.imshow('frame',frame)
 
         if i < 240:
+            # For 240 iterations the time and number of cars detected in each frame are stored in the data arrays.
             time_arr[i] = time.time()
             carcount_arr[i]=len(cars)
             print ("number of cars:" )
             print (carcount_arr[i])
             i += 1
 
-
+    # After ~5 mins the loop exits and a unique .hdf5 file is created.
     datafile = str(math.floor(time.time())) + '.hdf5'
     with h5py.File(datafile,'w') as f:
+        # The data arrays containing the cars per frame and time of each frame are written into the .hdf5 file
         f.create_dataset('carcount', data=carcount_arr)
         f.create_dataset('time', data=time_arr)
 
-
+    # This file was then read from and printed so that the user could verify that the data was recorded and output correctly.
     f = h5py.File(datafile,'r')
     getcardata = f['carcount'].value
     gettimedata = f['time'].value
@@ -73,7 +100,8 @@ while True:
     print(gettimedata)
 
 
-
+    # Release the videocapture object and file
     cap.release()
     out.release()
+    # Close all of the frames.
     cv2.destroyAllWindows()
--- PostProcessCarDetection/postprocess_car_detection.py
+++ PostProcessCarDetection/postprocess_car_detection.py
@@ -1,61 +1,68 @@
-#import libraries of python opencv
+#
+# Final version of our post process car counting script.
+#
+# Created by Zachary Safran and Alecia Griffin.
+#
 import cv2
 import numpy as np
 import h5py
 import time
 import math
 
-#create VideoCapture object and read from video file
+# Create VideoCapture object and read from video file
 cap = cv2.VideoCapture('/home/pi/.virtualenvs/cv_wh/ProjectDocs/code/miniprojectData/1537376806/1537376806/1537376806.avi')
-#use trained cars XML classifiers
+# Use trained cars XML classifiers
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
-count=0
+count = 0
+# Array for data is preallocated.
 post_carcount = np.zeros(240)
 i = 0
 
-#read until video is completed
+# Read until video is completed.
 
 while True:
-    #capture frame by frame
+    # Capture frame by frame.
     ret, frame = cap.read()
-    #convert video into gray scale of each frames
+    # Convert video into gray scale of each frames.
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    #detect cars in the video
+    # Each frame is analyzed for cars.
     cars = car_cascade.detectMultiScale(gray, 1.1, 3)
 
-    #to draw arectangle in each cars 
+    # A GREEN rectangle is drawn around the detected cars and the variable count is incremented.
     for (x,y,w,h) in cars:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)      
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
         roi_color = frame[y:y+h, x:x+w]
         count+=1
         cv2.putText(frame, str(count),(470,400), cv2.FONT_ITALIC, 2,(0,255,0),2,cv2.LINE_AA)
-    #display the resulting frame
+    # Text of the cummulative car count is added to each frame.
+    # Video frame is shown.
     cv2.imshow('video', frame)
 
     if i < 240:
+        # For 240 iterations the number of cars detected in each frame are stored in the data array.
         post_carcount[i] =len(cars)
         print ("number of cars:")
-        print (post_carcount[i]) 
+        print (post_carcount[i])
         i += 1
-        
-    
-    #press Q on keyboard to exit
+
+
+    # Press Q on keyboard to exit.
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
-
+# After ~5 mins the loop exits and a unique .hdf5 file is created.
 datafile = str(math.floor(time.time())) + '.hdf5'
 with h5py.File(datafile,'w') as f:
+        # The data array containing the cars per frame is written into the .hdf5 file
         f.create_dataset('post_carcount', data=post_carcount)
-        
 
 
+# This file was then read from and printed so that the user could verify that the data was recorded and output correctly.
 f = h5py.File(datafile,'r')
 getcardata = f['post_carcount'].value
 print(getcardata)
 
-#release the videocapture object
+# Release the videocapture object.
 cap.release()
-#close all the frames
+# Close all of the frames.
 cv2.destroyAllWindows()
